e7a96f6a31e025683f0855b0f9bb8019153355e8
Simon Blaha <s.blaha@live.at>
2019-02-09 04:17:08 +0100

    modify filter function styles

--- js/filter-function.js
+++ js/filter-function.js
@@ -6,20 +6,24 @@ comedy = $(".Comedy");
 
 //display action only
 	$("#action-movies").on('click', function() {
-		action.fadeIn(300);
+		action.fadeIn(300).css("background-color", "lightgrey");
 		horror.fadeOut(300);
 		comedy.fadeOut(300);
+		$(this).css("background-color", "lightgrey");
 //display horror only
 	});
 	$("#horror-movies").on('click', function() {
-		horror.fadeIn(300);
+		horror.fadeIn(300).css("background-color", "lightgrey");
 		action.fadeOut(300);
 		comedy.fadeOut(300);
+		$(this).css("background-color", "lightgrey");
+		$("action-movies").css("background-color", "white")
 //display comedy only
 	});
 	$("#comedy-movies").on('click', function() {
-		comedy.fadeIn(300);
+		comedy.fadeIn(300).css("background-color", "lightgrey");
 		horror.fadeOut(300);
 		action.fadeOut(300);
+		$(this).css("background-color", "lightgrey");
 	});
 });
